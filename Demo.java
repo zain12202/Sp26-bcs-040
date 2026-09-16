79fa387be2120189a570c51bb7363db1e90af626
Sp26-bcs-040 <zainakram2812@gmail.com>
2026-09-16 15:06:17 +0500

    task 1 is added

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo{
+    public static void main(String args[]){
+        Student s1=new Student();
+        s1.name="John";
+        s1.age=34;
+        s1.completedCredits=120;
+        s1.printsummary();
+    }
+
+}
---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+     public static void main(String[] args) {
+            int age;
+            String name;
+            int completedCredits;
+            void printsummary() {
+                System.out.println("Name: " + name);
+                System.out.println("Age: " + age);
+                System.out.println("Completed Credits: " + completedCredits);
+            }
+     }
+}
