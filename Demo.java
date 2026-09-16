7beeaacb50afd6f357e03e345f0edf069560f69c
Sp26-bcs-040 <zainakram2812@gmail.com>
2026-09-16 15:27:46 +0500

    added lab2

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ public class Demo{
     public static void main(String args[]){
         Student s1=new Student();
         s1.name="John";
-        s1.age=34;
+        s1.id=001;
         s1.completedCredits=120;
         s1.printsummary();
     }
